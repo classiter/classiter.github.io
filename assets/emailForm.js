5480dacec002684ee4bd3fcb1330a4c9378c3088
classiter6289@gmail.com <classiter6289@gmail.com>
2020-11-14 15:35:30 -0500

    Fixed form

--- assets/emailForm.js
+++ assets/emailForm.js
@@ -8,23 +8,23 @@ $(document).ready(function(){
   });
   
   function email_form(){
-    let guest_email = $('#contactEmailForm').val(),
-        guest_message = $('#contactTextarea').val();
-        $.ajax({
-          url: "https://formspree.io/f/xaylbjak", 
-          method: "POST",
-          data: {email: guest_email,
-                message: guest_message,},
-          dataType: "json",
-          success: function(event){
-            //event.preventDefault();
-            $('#contactMe').hide();
-            $(".result").html("<div class=\"alert alert-success\">Your message was sent successfully. Thank you.</div>");
-          },
-          error: function(){
-            $(".result").html("<span class=\"alert alert-danger\">Something went wrong or was not filled in correctly. Please try again.</span>");
-          }
-        });
+    let guest_email = $('#email').val(),
+        guest_message = $('#message').val();
+    $.ajax({
+      url: "https://formspree.io/f/xaylbjak", 
+      method: "POST",
+      data: {email: guest_email,
+             message: guest_message,},
+      dataType: "json",
+      success: function(event){
+        //event.preventDefault();
+        $('#contactMe').hide();
+        $(".result").html("<div class=\"alert alert-success\">Your message was sent successfully. Thank you.</div>");
+      },
+      error: function(){
+        $(".result").html("<span class=\"alert alert-danger\">Something went wrong or was not filled in correctly. Please try again.</span>");
+      }
+    });
   }
 
   $("#submitButton").on("click",function(){
