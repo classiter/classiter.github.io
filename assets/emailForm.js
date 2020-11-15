4278efca122cd6b80fd73e2bb6cb6c720f068a22
classiter6289@gmail.com <classiter6289@gmail.com>
2020-11-15 18:23:02 -0500

    Updated email form code

--- assets/emailForm.js
+++ assets/emailForm.js
@@ -1,34 +1,56 @@
-$(document).ready(function(){
-  var $toggle = $('.navbar-burger');
-  var $menu = $('.navbar-menu');
-
-  $toggle.click(function() {
-    $(this).toggleClass('is-active');
-    $menu.toggleClass('is-active');
+$(document).ready(function () {
+  // Mobile meny JS
+  var $toggle = $(".navbar-burger");
+  var $menu = $(".navbar-menu");
+  $toggle.click(function () {
+    $(this).toggleClass("is-active");
+    $menu.toggleClass("is-active");
   });
-  
-  function email_form(){
-    let guest_email = $('#email').val(),
-        guest_message = $('#message').val();
-    $.ajax({
-      url: "https://formspree.io/f/xaylbjak", 
-      method: "POST",
-      data: {email: guest_email,
-             message: guest_message,},
-      dataType: "json",
-      success: function(event){
-        //event.preventDefault();
-        $('#contactMe').hide();
-        $(".result").html("<div class=\"alert alert-success\">Your message was sent successfully. Thank you.</div>");
-      },
-      error: function(){
-        $(".result").html("<span class=\"alert alert-danger\">Something went wrong or was not filled in correctly. Please try again.</span>");
-      }
+
+  // Email Form JS
+  window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in your form HTML above
+
+    var form = document.getElementById("mainForm");
+    var button = document.getElementById("mainFormSubmit");
+    var status = document.getElementById("mainFormResult");
+
+    // Success and Error functions for after the form is submitted
+
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.classList.remove("is-invisible");
+      status.innerHTML = "Your message was sent successfully. Thank you!";
+    }
+
+    function error() {
+      status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
     });
-  }
-
-  $("#submitButton").on("click",function(){
-    email_form();
   });
 
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
 });
